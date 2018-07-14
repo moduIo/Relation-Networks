96ef5105a1ecea4131ddaa29a496028c4faa30d2
Tim <ethosscream@yahoo.com>
2018-07-13 22:16:27 -0600

    Fix many dependency errors

--- CLEVR.py
+++ CLEVR.py
@@ -3,6 +3,7 @@
 # https://arxiv.org/pdf/1706.01427.pdf
 #
 from __future__ import print_function
+import random as ra
 import tensorflow as tf
 import numpy as np
 import keras
@@ -12,9 +13,9 @@ from keras.layers import Input, Dense, Dropout, Reshape, Lambda, Embedding, LSTM
 from keras.models import Model
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing import sequence
-from random import *
 import json
 import os.path
+from scipy import ndimage, misc
 
 #
 # Loads CLEVR dataset.
@@ -23,7 +24,7 @@ def load_data(split, n, vocab_size):
 	path = '../../Datasets/CLEVR_v1.0'
 	questions_path = path + '/questions/CLEVR_' + split + '_questions.json'
 	subset_questions_path = path + '/questions/CLEVR_' + split + '_questions_' + str(n) + '.json'
-	images_path = path + '/images'
+	images_path = path + '/images/' + split + '/'
 	x_text = []
 	x_image = []
 	y = []
@@ -31,6 +32,8 @@ def load_data(split, n, vocab_size):
 	num_labels = 0
 
 	# Attempt to load saved JSON subset of the questions
+	print('Loading data...')
+
 	if os.path.exists(subset_questions_path):
 		with open(subset_questions_path) as f:
 			data = json.load(f)
@@ -43,9 +46,11 @@ def load_data(split, n, vocab_size):
 
 		print('JSON subset saved to file...')
 
-	print('Data loaded...')
+	print('Data loaded.')
 
-	# Process data
+	# Store data
+	print('Storing data...')
+
 	for q in data[0:n]:
 		# Create an index for each answer
 		if not q['answer'] in labels:
@@ -53,20 +58,29 @@ def load_data(split, n, vocab_size):
 			num_labels += 1
 
 		x_text.append(q['question'])
+		x_image.append(misc.imread(images_path + q['image_filename'], mode='RGB'))
 		y.append(labels[q['answer']])
 
+	print('Data stored.')
+
 	# Convert question corpus into sequential encoding for LSTM
+	print('Processing data...')
 	t = Tokenizer(num_words=vocab_size)
 	t.fit_on_texts(x_text)
 	sequences = t.texts_to_sequences(x_text)
 	x_text = sequence.pad_sequences(sequences, maxlen=vocab_size)
 
+	# Convert x_image to np array
+	x_image = np.array(x_image)
+
 	# Convert labels to categorical labels
 	y = keras.utils.to_categorical(y, num_labels + 1)
 
-	print(x_text.shape)
-	print(y.shape)
-	exit()
+	print('Text: ', x_text.shape)
+	print('Image: ', x_image.shape)
+	print('Labels: ', y.shape)
+
+	return ([x_text, x_image], y), num_labels
 
 #
 # Preprocesses the input image by cropping and random rotations.
@@ -74,10 +88,10 @@ def load_data(split, n, vocab_size):
 def process_image(x):
 	target_height, target_width = 128, 128
 	rotation_range = .05  # In radians
-	degs = random.uniform(-rotation_range, rotation_range)
+	degs = ra.uniform(-rotation_range, rotation_range)
 
 	x = tf.image.resize_image_with_crop_or_pad(x, target_height, target_width)
-	x = tf.image.rotate(x, degs)
+	x = tf.contrib.image.rotate(x, degs)
 
 	return x
 
@@ -95,8 +109,8 @@ def get_relation_vectors(x):
 
 	# Get k unique random objects
 	while k > 0:
-		i = randint(0, shape[1] - 1)
-		j = randint(0, shape[2] - 1)
+		i = ra.randint(0, shape[1] - 1)
+		j = ra.randint(0, shape[2] - 1)
 
 		if not (i, j) in keys:
 			keys.append((i, j))
@@ -117,34 +131,26 @@ def get_relation_vectors(x):
 batch_size = 64
 epochs = 12
 learning_rate = .00025
-img_rows, img_cols = 28, 28  # input image dimensions
+img_rows, img_cols = 320, 480
+image_input_shape = (img_rows, img_cols, 1)
 vocab_size = 1000
 
 #
 # Load & Preprocess CLEVR
 #
-(x_train, y_train), num_classes = load_data('train', 2000, vocab_size)
-
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    image_input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    image_input_shape = (img_rows, img_cols, 1)
+(x_train, y_train), num_labels = load_data('train', 2000, vocab_size)
 
 #
 # Define LSTM
 #
-text_inputs = Input(shape=text_input_shape)
-text_x = Embedding(max_text_features, 128)(text_inputs)
+text_inputs = Input(shape=(vocab_size,), name='text_input')
+text_x = Embedding(vocab_size, 128)(text_inputs)
 text_x = LSTM(128)(text_x)
 
 #
 # Define CNN
 #
-image_inputs = Input(shape=image_input_shape)
+image_inputs = Input(shape=image_input_shape, name='image_input')
 image_x = Lambda(process_image)(image_inputs)
 image_x = Conv2D(24, kernel_size=(3, 3), strides=2, activation='relu')(image_x)
 image_x = Conv2D(24, kernel_size=(3, 3), strides=2, activation='relu')(image_x)
@@ -155,7 +161,7 @@ shape = K.int_shape(image_x)
 #
 # Define Relation Network layer
 #
-RN_inputs = Input(shape=(1, 2 * shape[3]))
+RN_inputs = Input(shape=(1, (2 * shape[3]) + K.int_shape(text_x)[1]))
 RN_x = Dense(256, activation='relu')(RN_inputs)
 RN_x = Dense(256, activation='relu')(RN_x)
 RN_x = Dense(256, activation='relu')(RN_x)
@@ -168,7 +174,7 @@ RN = Model(inputs=RN_inputs, outputs=RN_outputs)
 #
 relations = Lambda(get_relation_vectors)(image_x)           # Get tensor [batch, relation_ID, relation_vectors]
 question = RepeatVector(K.int_shape(relations)[1])(text_x)  # Shape question vector to same size as relations
-relations = Concatenate(axis=1)([relations, question])      # Merge tensors [batch, relation_ID, relation_vectors, question_vector]
+relations = Concatenate(axis=2)([relations, question])      # Merge tensors [batch, relation_ID, relation_vectors, question_vector]
 g = TimeDistributed(RN)(relations)                          # TimeDistributed applies RN to relation vectors.
 g = Lambda(lambda x: K.sum(x, axis=1))(g)                   # Sum over relation_ID
 
@@ -180,12 +186,12 @@ f = Dropout(.5)(f)
 f = Dense(256, activation='relu')(f)
 f = Dropout(.5)(f)
 f = Dense(29, activation='relu')(f)
-outputs = Dense(num_classes, activation='softmax')(f)
+outputs = Dense(num_labels, activation='softmax')(f)
 
 #
 # Train model
 #
-model = Model(inputs=inputs, outputs=outputs)
+model = Model(inputs=[text_inputs, image_inputs], outputs=outputs)
 print(model.summary())
 
 model.compile(optimizer=Adam(lr=learning_rate),
